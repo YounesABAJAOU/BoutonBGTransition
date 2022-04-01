367bcaa3e1037250fabfb4d09d5b5176ceda20e1
YounesABAJAOU <younes-abajaou14@hotmail.com>
2022-04-01 09:20:22 +0200

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+let button = document.getElementById("button")
+
+// button.addEventListener("click", function(){
+//     carre.style.opacity = "0";
+// })
+
+// button.addEventListener("click", function(){
+//     carre.style.display = "none"
+// });
+
+// button.addEventListener("click", function(){
+//     carre.body.classList.toggle("mystyle");
+// });
+
+button.addEventListener("click", function(){
+    document.body.classList.toggle("mystyle");
+});
